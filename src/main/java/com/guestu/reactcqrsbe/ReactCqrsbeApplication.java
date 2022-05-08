a90435eeb5f60943436f5ce1cd3075eb8c8f1d91
Abdoulaye Diaw <abdoulaye.diaw@joza-it.fr>
2022-05-08 10:57:24 +0200

    Initial Commit

---
+++ src/main/java/com/guestu/reactcqrsbe/ReactCqrsbeApplication.java
@@ -0,0 +1,13 @@
+package com.guestu.reactcqrsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactCqrsbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactCqrsbeApplication.class, args);
+    }
+
+}
